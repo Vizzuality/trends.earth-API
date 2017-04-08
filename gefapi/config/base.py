6499eb1ec7e68c6365d60e6cdfb669eb247f19b3
rrequero <rrequero@gmail.com>
2017-04-08 13:36:46 +0200

    fix problems deploy

--- gefapi/config/base.py
+++ gefapi/config/base.py
@@ -17,7 +17,7 @@ SETTINGS = {
     'SQLALCHEMY_DATABASE_URI': 'postgresql://'+os.getenv('DATABASE_ENV_POSTGRES_USER')+':'+os.getenv('DATABASE_ENV_POSTGRES_PASSWORD')+'@'+os.getenv('DATABASE_PORT_5432_TCP_ADDR')+':'+os.getenv('DATABASE_PORT_5432_TCP_PORT')+'/'+os.getenv('DATABASE_ENV_POSTGRES_DB'),
     'SECRET_KEY': 'mysecret',
     'DOCKER_URL': os.getenv('DOCKER_URL'),
-    'REGISTRY_URL': 'localhost:'+os.getenv('REGISTRY_PORT_5000_TCP_PORT'),
+    'REGISTRY_URL': 'localhost:'+os.getenv('REGISTRY_PORT_5000_TCP_PORT', ''),
     'UPLOAD_FOLDER': '/tmp/scripts',
     'ALLOWED_EXTENSIONS': set(['tar.gz']),
     'JWT_AUTH_USERNAME_KEY': 'email',
